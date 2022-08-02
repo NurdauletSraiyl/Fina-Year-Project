b99ca8ca71c2940de65ae29af8b77c0cf9240577
nurdaulet.sraiyl@samgau.com <Mvie9LF5sM>
2022-08-03 00:58:59 +0600

    Brookes Authorization Test

--- Project-Brookes/Tests.cs
+++ Project-Brookes/Tests.cs
@@ -1,15 +1,60 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
 using NUnit.Framework;
 
 namespace Project_Brookes
 {
     [TestFixture]
-    public class Tests
+    public class AuthorizationTestTest
     {
-        [Test]
-        public void Test1()
+        private IWebDriver driver;
+        public IDictionary<string, object> vars { get; private set; }
+        private IJavaScriptExecutor js;
+
+        [SetUp]
+        public void SetUp()
         {
-            Assert.True(true);
+            driver = new ChromeDriver();
+            js = (IJavaScriptExecutor) driver;
+            vars = new Dictionary<string, object>();
+        }
+
+        [TearDown]
+        protected void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void authorizationTest()
+        {
+            driver.Navigate().GoToUrl("https://www.brookes.ac.uk/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1552, 840);
+            driver.FindElement(By.LinkText("Students")).Click();
+            {
+                var element = driver.FindElement(By.LinkText("Students"));
+                Actions builder = new Actions(driver);
+                builder.MoveToElement(element).Perform();
+            }
+            driver.FindElement(By.Id("quicklinks-item1")).Click();
+            driver.FindElement(By.Id("username")).Click();
+            driver.FindElement(By.Id("username")).SendKeys("17010029");
+            driver.FindElement(By.Id("password")).Click();
+            driver.FindElement(By.Id("password")).SendKeys("441814Nurik!");
+            driver.FindElement(By.Name("_eventId_proceed")).Click();
+            Thread.Sleep(8000);
+            driver.FindElement(By.Id("user")).Click();
+            driver.FindElement(By.Id("signOut")).Click();
+            driver.Close();
         }
     }
 }
